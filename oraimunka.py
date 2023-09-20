246ac075de60ff7105715b75324ae25c7fa186d7
Balku <144911108+balkuu@users.noreply.github.com>
2023-09-20 14:17:05 +0200

    Add files via upload

---
+++ oraimunka.py
@@ -0,0 +1,56 @@
+#kérjünk be 5db egész számot a felhasználótol, pozitivak-e vagy negatívak, hány az 5 közül nagyobb mint 20
+
+szamok1 = int(input("Kérjük adjon meg 1 számot:"))
+
+if szamok1 > 0:
+    print("Az első szám pozitív")
+else:
+    print("Az első szám negatív")
+if szamok1 > 20:    
+    print("Az első szám nagyobb mint 20!")
+else:
+    print("Az első szám kisebb mint 20!")
+
+szamok2 = int(input("Kérjük adjon meg még 1 számot:"))
+
+if szamok2 > 0:
+    print("A második szám pozitív")
+else:
+    print("A második szám negatív")
+if szamok2 > 20:    
+    print("A második szám nagyobb mint 20!")
+else:
+    print("A második szám kisebb mint 20!")
+    
+szamok3 = int(input("Kérjük adjon meg még 1 számot:"))
+
+if szamok3 > 0:
+    print("A harmadik szám pozitív")
+else:
+    print("A harmadik szám negatív")
+if szamok3 > 20:    
+    print("A harmadik szám nagyobb mint 20!")
+else:
+    print("A harmadik szám kisebb mint 20!")
+szamok4 = int(input("Kérjük adjon meg még 1 számot:"))
+
+if szamok4 > 0:
+    print("A negyedik szám pozitív")
+else:
+    print("A negyedik szám negatív")
+if szamok4 > 20:    
+    print("A negyedik szám nagyobb mint 20!")
+else:
+    print("A negyedik szám kisebb mint 20!")
+
+szamok5 = int(input("Kérjük adjon meg még 1 számot:"))
+
+if szamok5 > 0:
+    print("Az ötödik szám pozitív")
+else:
+    print("A ötödik szám negatív")
+if szamok1 > 20:    
+    print("Az ötödik szám nagyobb mint 20!")
+else:
+    print("Az ötödik szám kisebb mint 20!")
+
